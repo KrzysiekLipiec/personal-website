9f6f3cc80e8d2bbd668a470ab3270f8d38a87b13
Techpriest <krzysztof.kacper.lipiec@gmail.com>
2026-02-04 20:08:44 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My website!</h1>
+      <p>Welcome to my website. It is currently under construction</p>
+      <p>Thanks for stopping by!</p>
+    </>
+  );
+}
+
+export default App;
