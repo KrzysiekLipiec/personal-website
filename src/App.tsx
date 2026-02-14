d7a92bad6bd7113416f53a4a0629171126611b14
Techpriest <krzysztof.kacper.lipiec@gmail.com>
2026-02-14 18:09:35 +0100

    testing update on merge #11

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>My website!</h1>
+      <h1>Website!</h1>
       <p>Welcome to my website. It is currently under construction</p>
       <p style={{ marginTop: "-13px" }}>Thanks for stopping by!</p>
     </>
